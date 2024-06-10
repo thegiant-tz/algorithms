7485b8716f463ab04a8ddd53c5d0bd7f258ccf0c
Shebyclassic2018 <shaabanbakhresa85@gmail.com>
2024-06-10 10:45:08 +0300

    bug fix

--- src/AES.php
+++ src/AES.php
@@ -7,30 +7,27 @@ class AES
     static function encrypt($data, $key, $iv)
     {
         // The encryption method to be used:
-        $iv = substr(hash( 'sha256', $iv), 0, 16);
-        $key = hash( 'sha256', $key);
-        $encrypt_method = "AES-256-CBC";
+        $encrypt_method = "aes-256-cbc";
+
+        // Encrypted payload:
         $encrypted = openssl_encrypt($data, $encrypt_method, $key, 0, $iv);
+        
         // Base 64 Encode the encrypted payload:
         $encryptedData = base64_encode($encrypted);
+        
         return $encryptedData;
     }
 
-    static function decrypt($encrypted_data, $key, $iv)
+    static function decrypt($base64_encrypted_payload, $key, $iv)
     {
-        $iv = substr(hash( 'sha256', $iv), 0, 16);
-        $key = hash( 'sha256', $key);
+        // The encryption method to be used:
         $method = "aes-256-cbc";
-        $decrypted = openssl_decrypt(base64_decode($encrypted_data), $method, $key, 0, $iv );
+
+        // Base 64 Decode to get encrypted payload
+        $encrypted_data = base64_decode($base64_encrypted_payload);
+
+        // Decrypted payload
+        $decrypted = openssl_decrypt($encrypted_data, $method, $key, 0, $iv );
         return $decrypted;
-    }
-
-
-    public static function hashedKey($key) {
-        return hash('sha256', $key);
-    }
-
-    public static function hashedIv($key) {
-        return self::hashedKey($key);
     }
 }
